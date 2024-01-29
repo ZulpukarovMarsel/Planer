7776a2215e2d8153ec324d86210ab560df7dca5d
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-01-29 22:28:33 +0600

    update event

--- routes/events.py
+++ routes/events.py
@@ -53,3 +53,23 @@ async def delete_event(id: int) -> dict:
 async def delete_all_events() -> dict:
     events.clear()
     return {"message": "Events deleted successfully!"}
+
+
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(id: int, new_data: Event, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
+
