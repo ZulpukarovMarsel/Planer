9f606a5a9c91cc4acbd4ac8e540a9087e0792ce9
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-01-29 22:44:30 +0600

    update model Event User

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlmodel import SQLModel, Session, create_engine
 from models.events import Event
+from models.users import User
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: Optional[str] = None
+
+
+async def initialize_database(self):
+    client = AsyncIOMotorClient(self.DATABASE_URL)
+    await init_beanie(
+        database=client.get_default_database(),
+        document_models=[Event, User])
+
+
+class Config:
+    env_file = ".env"
 
 
 database_file = "planner.db"
@@ -9,8 +29,10 @@ database_connection_string = f"sqlite:///{database_file}"
 connect_args = {"check_same_thread": False}
 engine_url = create_engine(database_connection_string, echo=True, connect_args=connect_args)
 
+
 def conn():
     SQLModel.metadata.create_all(engine_url)
+
 
 def get_session():
     with Session(engine_url) as session:
