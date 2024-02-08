f9bd128e9d5759ceb6f60b5de4f07256ebac6684
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-08 18:42:39 +0600

    update events.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-from fastapi import FastAPI
-from routes.users import user_router
-from routes.events import event_router
 import uvicorn
-from database.connection import conn
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from database.connection import Settings
+from routes.events import event_router
+from routes.users import user_router
+
 app = FastAPI()
+
+settings = Settings()
+
+# Register routes
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 
+
 @app.on_event("startup")
-def on_startup():
-    conn()
+async def init_db():
+    await settings.initialize_database()
+
 
 @app.get("/")
 async def home():
     return RedirectResponse(url="/event/")
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000,
-                reload=True)
-
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
