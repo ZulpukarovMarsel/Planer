45aa5c52d3621c50d0f10490864f9fd1e2a118ff
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-08 18:41:32 +0600

    update models(users.py)

--- models/users.py
+++ models/users.py
@@ -1,33 +1,24 @@
-from typing import Optional, List
-from beanie import Document, Link
+from beanie import Document
+
 from pydantic import BaseModel, EmailStr
-from models.events import Event
 
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
-    events: Optional[List[Event]]
 
     class Settings:
         name = "users"
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "username": "strong!!!",
-                "events": [],
-            }
-        }
-class UserSignIn(BaseModel):
-    email: EmailStr
-    password: str
 
     class Config:
         schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
                 "password": "strong!!!",
-                "events": [],
             }
         }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
