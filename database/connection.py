d6870b4328d4ed07f0be9152e10e70f35bd3a706
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-08 18:40:45 +0600

    update data_base(connection.py)

--- database/connection.py
+++ database/connection.py
@@ -1,39 +1,60 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlmodel import SQLModel, Session, create_engine
+from typing import Any, List, Optional
+
+from beanie import init_beanie, PydanticObjectId
 from models.events import Event
 from models.users import User
-from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from typing import Optional
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 
 
 class Settings(BaseSettings):
     DATABASE_URL: Optional[str] = None
 
+    async def initialize_database(self):
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+        await init_beanie(database=client.get_default_database(),
+                          document_models=[Event, User])
 
-async def initialize_database(self):
-    client = AsyncIOMotorClient(self.DATABASE_URL)
-    await init_beanie(
-        database=client.get_default_database(),
-        document_models=[Event, User])
-
-
-class Config:
-    env_file = ".env"
+    class Config:
+        env_file = ".env"
 
 
-database_file = "planner.db"
+class Database:
+    def __init__(self, model):
+        self.model = model
 
-database_connection_string = f"sqlite:///{database_file}"
-connect_args = {"check_same_thread": False}
-engine_url = create_engine(database_connection_string, echo=True, connect_args=connect_args)
+    async def save(self, document) -> None:
+        await document.create()
+        return
 
+    async def get(self, id: PydanticObjectId) -> Any:
+        doc = await self.model.get(id)
+        if doc:
+            return doc
+        return False
 
-def conn():
-    SQLModel.metadata.create_all(engine_url)
+    async def get_all(self) -> List[Any]:
+        docs = await self.model.find_all().to_list()
+        return docs
 
+    async def update(self, id: PydanticObjectId, body: BaseModel) -> Any:
+        doc_id = id
+        des_body = body.dict()
 
-def get_session():
-    with Session(engine_url) as session:
-        yield session
+        des_body = {k: v for k, v in des_body.items() if v is not None}
+        update_query = {"$set": {
+            field: value for field, value in des_body.items()
+        }}
+
+        doc = await self.get(doc_id)
+        if not doc:
+            return False
+        await doc.update(update_query)
+        return doc
+
+    async def delete(self, id: PydanticObjectId) -> bool:
+        doc = await self.get(id)
+        if not doc:
+            return False
+        await doc.delete()
+        return True
